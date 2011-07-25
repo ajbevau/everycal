d614e85c7fabecbdd7bd32d776493f25ac3ab26c
Andrew Bevitt <andrew.bevitt@unimelb.edu.au>
2011-07-25 17:01:00 +1000

    remove test cases from external calendar providers

--- includes/external-calendar-providers.php
+++ includes/external-calendar-providers.php
@@ -9,10 +9,6 @@ require( ECP1_DIR . '/includes/check-ecp1-defined.php' );
 // Define the calendar providers
 $_ecp1_calendars = array(
 	'google' => array( 'name' => 'Google Calendar', 'fullcal_plugin' => 'gcal.js' ),
-	'test1' => array( 'name' => 'Test Cal 1', 'fullcal_plugin' => 'gcal.js' ), # For testing settings ONLY
-	'test2' => array( 'name' => 'Test Cal 2', 'fullcal_plugin' => 'gcal.js' ), # For testing settings ONLY
-	'test3' => array( 'name' => 'Test Cal 3', 'fullcal_plugin' => 'gcal.js' ), # For testing settings ONLY
-	'test4' => array( 'name' => 'Test Cal 4', 'fullcal_plugin' => 'gcal.js' ), # For testing settings ONLY
 );
 
 // Function that shamelessly returns the above array so we don't have to global it
