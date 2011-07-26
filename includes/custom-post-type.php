92081cc503a6432c73df5bd8746301ebaad5f958
Andrew Bevitt <andrew@andrewbevitt.com>
2011-07-27 00:46:47 +1000

    definitions for capability types and skeleton of a map_meta_cap filter to allow calendar editors to edit/delete events in their calendars

--- includes/custom-post-type.php
+++ includes/custom-post-type.php
@@ -6,6 +6,16 @@
 // Make sure we're included from within the plugin
 require( ECP1_DIR . '/includes/check-ecp1-defined.php' );
 
+// Define the capability types for the two custom post types
+// if you DO NOT want to use a role manager then set these
+// to a existing WordPress type: I recommend page for calendar
+// and post for events.
+// 
+// If you want more fine grained access control then change 
+// these to something else and setup the capability/roles.
+define( 'ECP1_CALENDAR_CAP', 'page' );
+define( 'ECP1_EVENT_CAP', 'post' );
+
 // Add action hooks
 add_action( 'init', 'ecp1_register_types' );
 
@@ -48,11 +58,11 @@ function ecp1_register_types() {
 		'public' => true,
 		'exclude_from_search' => true, # don't show events unless the plugin says to
 		'show_ui' => true,
-		#'menu_position' => 30,
 		'show_in_menu' => 'edit.php?post_type=ecp1_event',
-		# capabilities meta which will need a role manager to allow access
-		'capability_type' => 'ecp1_calendar',
-		'supports' => array( 'title' ),
+		# capabilities meta which will need a role manager if not default
+		'capability_type' => ECP1_CALENDAR_CAP,
+		'map_meta_cap' => true, # make sure all meta capabilities are mapped
+		'supports' => array( 'title', 'author' ),
 		'rewrite' => array( 'slug' => 'calendar' ),
 		'show_in_nav_menus' => false,
 	);
@@ -65,9 +75,10 @@ function ecp1_register_types() {
 		'exclude_from_search' => true, # don't show events unless the plugin says to
 		'show_ui' => true,
 		'menu_position' => 30,
-		#'show_in_menu' => 'edit.php?post_type=ecp1_calendar',
-		'capability_type' => 'post', # capabilities match posts
-		'supports' => array( 'title', 'thumbnail' ),
+		# capabilities meta which will need a role manage if not default
+		'capability_type' => ECP1_EVENT_CAP,
+		'map_meta_cap' => true, # make sure all meta capabilities are mapped
+		'supports' => array( 'title', 'author', 'thumbnail' ),
 		'rewrite' => array( 'slug' => 'event' ),
 		'show_in_nav_menus' => false,
 	);
@@ -75,6 +86,24 @@ function ecp1_register_types() {
 	// Register the custom post type
 	register_post_type( 'ecp1_event', $ecp1_evt_args );
 	register_post_type( 'ecp1_calendar', $ecp1_cal_args );
+}
+
+// Now define a capbilities filter to allow editors of calendars
+// the ability to edit all events in that calendar (hopefully).
+add_filter( 'map_meta_cap', 'ecp1_map_calendar_cap_to_event', 100, 4 );
+function ecp1_map_calendar_cap_to_event( $caps, $cap, $user_id, $args ) {
+	
+	// Only proceed if we have a post argument (i.e. the event)
+	// and it actually is a post type of ecp1_event
+	$event_id = is_array( $args ) ? $args[0] : $args;
+	$event = get_post( $event_id );
+	if ( 'ecp1_event' == get_post_type( $post ) ) {
+		// TODO: look at $cap and $user_id and ecp1_calendar in post meta
+	}
+	
+	// Finally return the caps that are left over
+	return $caps;
+
 }
 
 ?>
--- includes/data/calendar-fields-admin.php
+++ includes/data/calendar-fields-admin.php
@@ -112,7 +112,7 @@ function ecp1_calendar_meta_form() {
 	// Check if local calendars can change event timezones
 	$disabled_str = _ecp1_get_option( 'tz_change' ) ? 'class="ecp1_select"' : 'class="ecp1_select" disabled="disabled"';
 	echo _ecp1_timezone_select( 'ecp1_timezone', $ecp1_tz, $disabled_str );
-	if ( ! is_null( $disabled_str ) )
+	if ( ! _ecp1_get_option( 'tz_change' ) )
 		printf( '<em>%s</em>', __( 'Every Calendar +1 settings prevent change: WordPress TZ will be used.' ) );
 ?>
 				</td>
@@ -157,7 +157,7 @@ function ecp1_calendar_save() {
 		return $post->ID;
 	
 	// Verify the user can actually edit posts
-	if ( ! current_user_can( 'edit_post', $post->ID ) )
+	if ( ! current_user_can( 'edit_ecp1_calendar', $post->ID ) )
 		return $post->ID;
 	
 	// URL Encode the external URL
