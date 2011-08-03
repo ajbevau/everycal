07a823d67900c616594cfc5db2faf08d38abaa1c
Andrew Bevitt <andrew@andrewbevitt.com>
2011-08-03 07:46:47 +0000

    added uninstall hook

---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Every Calendar +1 Plugin
+ *
+ * Uninstall Script:
+ *  - Does nothing by design at present: no tables created and meta fields shouldn't be deleted just because uninstalling?
+ */
+
+
+// Make sure WordPress is uninstalling the plugin and then do the clean up
+if ( defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+
+	// Nothing
+
+}
+
+?>
