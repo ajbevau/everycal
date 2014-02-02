22d9132a041ff16d6ad435738c60a918b6eefd0d
Andrew Bevitt <andrew@andrewbevitt.com>
2014-02-02 09:40:28 +0900

    print <?xml so can upload to wp svn

--- ui/templates/rss-feed.php
+++ ui/templates/rss-feed.php
@@ -65,8 +65,9 @@ $my_id = $cal->ID; // because event meta reparses its calendars meta
 $feed_url = home_url() . '/ecp1/' . urlencode( $cal->post_name ) . '/events.rss';
 
 // ICAL HEADERS / CALENDAR DETAILS
+print('<?xml version="1.0" encoding="ISO-8859-1"?>');
 ?>
-<?xml version="1.0" encoding="ISO-8859-1"?>
+
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 	<atom:link href="<?php print $feed_url; ?>" rel="self" type="application/rss+xml" />
